62082c40d0a37835e090e379467916c7dcf07c80
MKS-students <mks.students@hackreactor.com>
2015-07-09 10:58:22 -0700

    rooks function working

--- src/solvers.js
+++ src/solvers.js
@@ -16,23 +16,58 @@
 // Return a matrix (an array of arrays) representing a single nxn chessboard,
 // with n rooks placed such that none of them can attack each other.
 window.findNRooksSolution = function(n) {
-  //solution array to hold all (var solution = [])
+  var board = new Board({n:n})
+  var rooks = 0;
+  var trackedRooks;
   var solution = [];
-  var row = [];
 
-  //var y is n arrays pushed into solution array
-  //for loop n times and push 0 into this array
-  for (var x = 0; x < n; x++){
-    row.push(0);
-  }
-  //var x is n indices pushed into each y array
-  //for loop n times pushing y into x
-  for (var y = 0; y < n; y++){
-    solution.push(row);
-  }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // for (i = 0; i < n; i++){
+  //   for (j = 0; j < n; j++){
+  //     board.setPiece(i, j, 1);
+  //     if (!board.hasColConflictAt(j)){
+
+  //     }
+
+  //     board.setPiece(i, j, 0);
+  //   }
+  // }
+  // return board.allRows;
+
+  //set up recursive function
+  var recurse = function(){
+    //base case
+    if (rooks === n) {
+      //loop through board all rows
+      _.each(board.allRows(), function(row) {
+        solution.push(row);
+      });
+      return;
+    }
+
+    //loop through row
+//   for (var i=0; i < n; i++) {
+      //loop through col
+      for(var j = 0; j < n; j++) {
+        board.setPiece(rooks, j, 1);
+        //increment rooks
+        rooks++;
+        if(board.hasAnyRooksConflicts()) {
+          board.setPiece(i, j, 0);
+          //increment rooks
+          rooks--;
+        }
+      }
+      recurse(board.allRows(rooks));
+    //}
+  }
+  recurse(board.allRows(0));
   return solution;
+
+
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // return solution;
+  ///maybe add later arrays to keep track of already used columns?
 };
 
 
