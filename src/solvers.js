55f5051be6bf0d9a8ccb72ad5b312c5a38cd5479
MKS-students <mks.students@hackreactor.com>
2015-07-09 15:51:46 -0700

    working rooks solution

--- src/solvers.js
+++ src/solvers.js
@@ -17,11 +17,7 @@
 // with n rooks placed such that none of them can attack each other.
 window.findNRooksSolution = function(n) {
 
-
-  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  ///maybe add later arrays to keep track of already used columns?
 };
-
 
 // Return the number of nxn chessboards that exist, with n rooks placed such that none
 // of them can attack each other.
@@ -30,72 +26,28 @@ window.countNRooksSolutions = function(n) {
   var rooks = 0;
   var solutionCount = 0;
 
-
-  //set up recursive function
-  // var recurse = function(board){
-  //   if (rooks === n) {
-  //     _.each(board.allRows(), function(rowarray) {
-  //       solution.push(rowarray);
-  //     });
-  //     return;
-  //   }
+  var recurse = function(){
 
 
-  var recurse = function(board, currentrow, currentcol){
-    if(rooks === n) {
-      solutionCount++;
-    }
-
-    for (var i = currentrow; i < n; i++) {
-      for(var j = currentcol; j < n; j++) {
+      var i = rooks;
+      for(var j = 0; j < n; j++) {
           board.setPiece(i, j, 1);
           rooks++;
         if(!board.hasAnyRooksConflicts()) {
-
-          recurse(board, i, j);
+          if(rooks === n) {
+            solutionCount++;
+            board.setPiece(i, j, 0);
+            rooks--;
+            continue;
+          }
+          recurse();
         }
-        // if (board.hasAnyRooksConflicts()) {
           board.setPiece(i, j, 0);
           rooks--;
-        // }
       }
-    }
+
   }
-  recurse(board, 0, 0);
-
-
-  //   var board = new Board({n:n});
-  // var rooks = 0;
-  // var trackedRooks;
-  // var solution = [];
-
-  // //set up recursive function
-  // var recurse = function(){
-  //   if (rooks === n) {
-  //     _.each(board.allRows(), function(row) {
-  //       solution.push(row);
-  //     });
-  //     return;
-  //   }
-
-  //   for (var i=0; i < n; i++) {
-  //     for(var j = 0; j < n; j++) {
-  //         board.setPiece(i, j, 1);
-  //         rooks++;
-  //       if(!board.hasAnyRooksConflicts()) {
-  //         recurse(board);
-
-  //       }
-  //         board.setPiece(i, j, 0);
-  //         rooks--;
-
-  //     }
-  //   }
-  // }
-  // recurse(board);
-  // return solution;
-
-  //console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  recurse();
   return solutionCount;
 };
 
